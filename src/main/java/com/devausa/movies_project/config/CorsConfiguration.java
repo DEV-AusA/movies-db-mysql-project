6243dd087a6514c1b628aaddb119246aec7f77fc
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-12 20:01:29 -0300

    enable CORS

---
+++ src/main/java/com/devausa/movies_project/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.devausa.movies_project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //cambiarlo para permitir una ip especifica
+                .allowedOrigins("*")
+//                .allowedOrigins("http://127.0.0.1:5501")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*");
+    }
+}
